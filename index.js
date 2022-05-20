1eebd8fa84ccebb3d4acf279b7aef60f88f33c12
Michael Dear <michaelcdear@gmail.com>
2022-05-20 17:37:34 -0400

    added dropdown menus to other navbar elements

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ const navbar4 = document.getElementById("personal")
 const navbar5 = document.getElementById("experience")
 const navbar6 = document.getElementById("contact")
 
-const navbar1menu = document.createElement("div")
+const navbarmenu = document.createElement("div")
 
 let menuToggle = 0
 
-function addEventListenerToElementByID (elementID, appendLocation, appendage, menuToggle) {
-    toggleMenu(elementID, appendLocation, appendage, menuToggle);
+function addEventListenerToElementByID (elementID, appendLocation, appendage, leftMargin) {
+    toggleMenu(elementID, appendLocation, appendage, leftMargin);
     }
 
-function toggleMenu(elementID, appendLocation, appendage, menuToggle) {
+function toggleMenu(elementID, appendLocation, appendage, leftMargin) {
     elementID.addEventListener("click", function (event) {
         if (event = PointerEvent) {
 
             if (menuToggle === 0) {
-                navbarpopup(appendLocation, appendage);
+                navbarpopup(appendLocation, appendage, leftMargin);
                 menuToggle = 1
                 return menuToggle
             } 
@@ -35,7 +35,7 @@ function toggleMenu(elementID, appendLocation, appendage, menuToggle) {
     })
 }
 
-function navbarpopup(appendLocation, appendage) {
+function navbarpopup(appendLocation, appendage, leftMargin) {
     appendLocation.append(appendage);
 
     appendage.style.backgroundColor = "aliceblue"
@@ -47,11 +47,16 @@ function navbarpopup(appendLocation, appendage) {
     appendage.style.borderColor = "black"
     appendage.style.borderRadius = "6px"
 
-    appendage.style.marginLeft = "1%"
+    appendage.style.marginLeft = leftMargin
     appendage.style.marginTop = "1%"
 }
 
-addEventListenerToElementByID(navbar1, navbar, navbar1menu, menuToggle);
+addEventListenerToElementByID(navbar1, navbar, navbarmenu, "1%");
+addEventListenerToElementByID(navbar2, navbar, navbarmenu, "14.5%");
+addEventListenerToElementByID(navbar3, navbar, navbarmenu, "31.7%");
+addEventListenerToElementByID(navbar4, navbar, navbarmenu, "48.5%");
+addEventListenerToElementByID(navbar5, navbar, navbarmenu, "65.3%");
+addEventListenerToElementByID(navbar6, navbar, navbarmenu, "85.6%");
 
 //navbarpopup(navbar, navbar1menu);
 
